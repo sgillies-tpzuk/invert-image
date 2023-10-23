88044eafb6d97ebbb0d60257d93b22df3f7ecba9
Steven Gillies <steven.gillies@telespazio.com>
2023-10-23 11:41:53 +0100

    added cli

--- invert-image/__main__.py
+++ invert-image/__main__.py
@@ -1,10 +1,33 @@
+from pathlib import Path
+from typing import Optional
+
+import click
 from PIL import Image, ImageOps
 
 
-def main() -> None:
-    image = Image.open("mountains.jpg")
+@click.command()
+@click.argument("filename", type=click.Path(exists=True))
+@click.option("-d", "--out-dir", "output_dir", type=click.Path(), default=None)
+@click.option("-o", "--out-file", "output_filename", type=str, default=None)
+def main(
+    filename: str, output_dir: Optional[str], output_filename: Optional[str]
+) -> None:
+    filepath = Path(filename)
+    output_dir: Path = Path.cwd() if output_dir is None else Path(output_dir)
+    output_filename: Path = output_filename or f"{filepath.stem}-inverted.jpg"
+
+    if not output_dir.exists():
+        Path(output_dir).mkdir()
+
+    output_filepath = (
+        output_filename
+        if output_dir is None
+        else Path.joinpath(output_dir, output_filename)
+    )
+
+    image = Image.open(filepath)
     inverted = ImageOps.invert(image)
-    inverted.save("mountains-inverted.jpg")
+    inverted.save(output_filepath)
 
 
 if __name__ == "__main__":
